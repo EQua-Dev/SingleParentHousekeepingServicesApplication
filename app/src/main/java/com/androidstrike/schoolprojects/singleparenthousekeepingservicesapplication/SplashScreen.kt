08b6416ea19ee684d0d76812347d0f1566bc06b5
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2023-08-17 11:12:41 +0100

    implemented changes

---
+++ app/src/main/java/com/androidstrike/schoolprojects/singleparenthousekeepingservicesapplication/SplashScreen.kt
@@ -0,0 +1,55 @@
+package com.androidstrike.schoolprojects.singleparenthousekeepingservicesapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.androidstrike.schoolprojects.singleparenthousekeepingservicesapplication.databinding.FragmentSplashScreenBinding
+
+class SplashScreen : Fragment() {
+   
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get()= _binding!!
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        with(binding) {
+            txtSplash.animate().setDuration(2000).alpha(1f).withEndAction {
+                val navToSignIn = SplashScreenDirections.actionSplashScreenToLandingFragment()
+                findNavController().navigate(navToSignIn)
+//
+//                if (auth.currentUser != null) {
+//                    // User is logged in
+//                    // Perform necessary actions
+//                    val navToHome = SplashScreenDirections.actionSplashScreenToClientHome(resources.getString(R.string.client))
+//                    findNavController().navigate(navToHome)
+//                } else {
+//                    // User is not logged in
+//                    // Redirect to login screen or perform other actions
+//
+//                }
+
+
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
